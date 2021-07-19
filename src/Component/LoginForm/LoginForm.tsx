4c4b2f33a17850d725263f7b325af40bc53c221b
Prakhar Varshney <70899048+PrakharV10@users.noreply.github.com>
2021-07-19 21:32:32 +0530

    Feat : Guest Credentials Support

--- src/Component/LoginForm/LoginForm.tsx
+++ src/Component/LoginForm/LoginForm.tsx
@@ -51,42 +51,57 @@ function LoginForm() {
 	}
 
 	return (
-		<form onSubmit={(e) => submitHandler(e)} className="text-center w-full px-7">
-			<header className="hidden mb-8 text-white-100 text-light md:block md:text-2xl xl:text-3xl">
+		<form onSubmit={(e) => submitHandler(e)} className='text-center w-full px-7'>
+			<header className='hidden mb-8 text-white-100 text-light md:block md:text-2xl xl:text-3xl'>
 				Login
 			</header>
 			{localInput.errorMessage && (
-				<div className="w-full bg-red-600 text-white-100 h-14 font-light text-sm rounded-sm flex justify-center items-center mb-6 md:mb-5">
+				<div className='w-full bg-red-600 text-white-100 h-14 font-light text-sm rounded-sm flex justify-center items-center mb-6 md:mb-5'>
 					{localInput.errorMessage}
 				</div>
 			)}
 			<input
-				type="mail"
-				placeholder="Email"
-				className="input-field w-full mb-6 md:mb-5"
+				type='mail'
+				placeholder='Email'
+				className='input-field w-full mb-6 md:mb-5'
 				value={localInput.email}
 				onChange={(e) => setLocalInput({ ...localInput, email: e.target.value })}
 				required
 			/>
 			<input
-				type="password"
-				placeholder="Password"
+				type='password'
+				placeholder='Password'
 				value={localInput.password}
 				onChange={(e) => setLocalInput({ ...localInput, password: e.target.value })}
-				className="input-field w-full mb-7 md:mb-5"
+				className='input-field w-full mb-7 md:mb-5'
 				required
 			/>
 			<button
-				type="submit"
-				className="btn bg-blue-700 mb-7 hover:bg-blue-800 transition-colors"
+				type='submit'
+				className='btn bg-blue-700 mb-7 hover:bg-blue-800 transition-colors'
 			>
 				{localInput.loading ? `LOGGIN IN` : `LOGIN`}
 			</button>
+			<div className='text-white-100 opacity-90 font-light md:-mt-2 text-sm  mb-2'>
+				Fancy a Sneek peak? Use{' '}
+				<span
+					onClick={() =>
+						setLocalInput({
+							...localInput,
+							email: 'johndoe@gmail.com',
+							password: '123456',
+						})
+					}
+					className='opacity-100 text-blue-400 font-semibold cursor-pointer hover:text-blue-500'
+				>
+					Guest Credentials
+				</span>
+			</div>
 
-			<div className="text-sm text-white-100 font-regular opacity-70 mb-4 md:hidden">
+			<div className='text-sm text-white-100 font-regular opacity-70 mb-4 md:hidden'>
 				Don't have an account ?{' '}
-				<span className="font-semibold">
-					<Link to="/">Signup here.</Link>
+				<span className='font-semibold'>
+					<Link to='/'>Signup here.</Link>
 				</span>
 			</div>
 		</form>
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -18,28 +18,28 @@ function Login() {
 	}, [isUserLoggedIn]);
 
 	return (
-		<div className="min-w-screen min-h-screen flex flex-col md:flex-row">
-			<div className="bg-black-700 h-64 py-4 px-7 w-full flex flex-col justify-center items-center md:h-screen md:w-2/4 md:relative md:items-start xl:p-24 ">
-				<div className="logo w-full md:absolute md:top-8 md:left-8 xl:top-24 xl:left-24">
+		<div className='min-w-screen min-h-screen flex flex-col md:flex-row'>
+			<div className='bg-black-700 h-64 py-4 px-7 w-full flex flex-col justify-center items-center md:h-screen md:w-2/4 md:relative md:items-start xl:p-24 '>
+				<div className='logo w-full md:absolute md:top-8 md:left-8 xl:top-24 xl:left-24'>
 					sparrow
 				</div>
-				<div className="font-regular text-lg text-white-100 w-full pt-7 xl:text-2xl">
+				<div className='font-regular text-lg text-white-100 w-full pt-7 xl:text-2xl'>
 					Welcome Back!
 				</div>
-				<p className="text-sm font-light text-gray-300 pt-4 w-full md:mb-14 xl:w-4/5 xl:text-lg">
+				<p className='text-sm font-light text-gray-300 pt-4 w-full md:mb-14 xl:w-4/5 xl:text-lg'>
 					We know you loved your previous experience, and are back for some more...
 					<br />
 					Check out the new Quizzes we have in store for you. Did you beat your personal
 					best yet?
 				</p>
-				<div className="hidden text-white-100 opacity-80 md:block md:font-extralight md:text-sm xl:text-base xl:font-light">
+				<div className='hidden text-white-100 opacity-80 md:block md:font-extralight md:text-sm xl:text-base xl:font-light'>
 					Didn't get the above refrence?{' '}
-					<span className="md:font-normal xl:font-semibold">
-						<Link to="/">Signup now!!</Link>
+					<span className='md:font-normal xl:font-semibold'>
+						<Link to='/'>Signup now!!</Link>
 					</span>
 				</div>
 			</div>
-			<div className="bg-black-800 h-full flex-1 pt-12 md:flex md:w-2/4 md:h-screen md:flex-col md:justify-center md:items-center xl:px-20">
+			<div className='bg-black-800 h-full flex-1 pt-12 md:flex md:w-2/4 md:h-screen md:flex-col md:justify-center md:items-center xl:px-20'>
 				<LoginForm />
 			</div>
 		</div>
