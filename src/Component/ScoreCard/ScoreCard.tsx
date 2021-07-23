e5343b2a2144ef5f1be34fcddbb9237fcb4a3414
Prakhar Varshney <70899048+PrakharV10@users.noreply.github.com>
2021-07-23 23:49:36 +0530

    Fix : bug

--- src/Component/ScoreCard/ScoreCard.tsx
+++ src/Component/ScoreCard/ScoreCard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ScoreCard = ({ score, index }: ScoreCardProps) => {
 	return (
-		<div className="bg-black-700 rounded mx-4 mb-8 p-4 h-20 shadow-md md:w-4/5 md:m-auto md:mb-8 md:h-24 flex justify-between items-center">
+		<div className='bg-black-700 rounded mx-4 mt-8 p-4 h-20 shadow-md md:w-4/5 md:m-auto md:mb-8 md:h-24 flex justify-between items-center'>
 			<div>{index + 1}</div>
 			<div>{score.userId.username}</div>
 			<div>{score.latestScore}</div>
